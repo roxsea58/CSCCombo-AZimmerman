76412b1a4f0e40f5dc0d966d9e451956f381cbeb
Zimmerman <S02168929@ccc.ccofc.edu>
2023-09-01 16:04:55 -0600

    first commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,44 @@
+/*
+ * 
+ * ------------------------------
+ * |~Written by Amber Zimmerman~|
+ * ------------------------------
+ * original code do not steal >:c
+ */
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// TODO woo im typing anything i want
+		
+		System.out.println("Hello World. I am a pretty cool IDE.");
+		
+		System.out.println("Yo, I think this shortcut using 'sysout' + ctrl + space + enter is pretty coolio.");
+		
+		System.out.println("Ya dude, you can use ctrl + space to see a list of proposals! Enter uses the first one.");
+		
+		System.out.println("Far out.");
+		
+		System.out.println("Ya.");
+		
+		int num1 = 66;
+		
+		int num2 = 12;
+		
+		int total = num1 + num2;
+		
+		int feet = num1 / num2;
+		
+		int inches = num1 % num2;
+		
+		//% is modulus(sp?) - use to give remainder
+		
+		System.out.println("The sum of the two numbers is " + total + "." + " This is calculated by adding " + num1 +" and " + num2 +".");
+		
+		System.out.println("Divided, the numbers equal " + feet + " feet and " + inches + " inches.");
+	}
+
+}
