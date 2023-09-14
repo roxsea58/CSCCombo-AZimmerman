a300765c6972cdeb6ca18604e89bb2bd94f0b117
Zimmerman <S02168929@ccc.ccofc.edu>
2023-09-14 16:13:03 -0600

    Sep 14

---
+++ Loops/src/Loops.java
@@ -0,0 +1,41 @@
+public class Loops {
+
+	public static void main(String[] args) {
+
+		
+//		While Loop:		
+//---------------------------------------------------------------------------------------------------
+//
+//		int num = 1;
+//
+//		while (num <= 10) {
+//
+//			if (num == 1) {
+//
+//				System.out.println("Counting up to 10! You are at: " + num + "!");
+//
+//				num += 1;
+//
+//			}
+//
+//			else {
+//
+//				System.out.println("Counting up to 10! You are at: " + num + "!");
+//
+//				num += 1;
+//
+//			}
+//		}
+//	}
+//}
+//		For Loop:
+//---------------------------------------------------------------------------------------------------
+		
+		int num;
+		
+		for (num = 1; num <= 10; num += 1) {
+		
+		System.out.println("Counting up to 10! You are at: " + num + "!");
+		}
+	}
+}
